ff4fe03fa35c7f904f4f206107949f70218fbd3c
meghana123-k <kothakondameghana3@gmail.com>
2026-02-25 15:54:20 +0530

    ADded input title and priority into array

--- task-flow-project/src/components/TaskForm.tsx
+++ task-flow-project/src/components/TaskForm.tsx
@@ -1,18 +1,45 @@
-export function TaskForm() {
+import { useState } from "react";
+
+type Priority = "low" | "medium" | "high";
+
+type TaskFormProps = {
+  addTask: (text: string, priority: Priority) => void;
+};
+
+export function TaskForm({ addTask }: TaskFormProps) {
+  const [inputText, setInputText] = useState<string>("");
+  const [priority, setPriority] = useState<"low" | "medium" | "high">("low");
+
+  function submitTask(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    addTask(inputText, priority);
+    setInputText("");
+    setPriority("low");
+  }
   return (
     <>
       <div className="header">
         <h1>Smart Task Flow</h1>
       </div>
       <div className="input-container">
-        <form id="task-form">
+        <form id="task-form" onSubmit={submitTask}>
           <input
             id="task-input"
             type="text"
             placeholder="Enter task"
             required
+            value={inputText}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setInputText(event?.target.value)
+            }
           />
-          <select id="priority-select">
+          <select
+            id="priority-select"
+            value={priority}
+            onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+              setPriority(e.target.value as Priority)
+            }
+          >
             <option value="low">Low</option>
             <option value="medium">Medium</option>
             <option value="high">High</option>
--- task-flow-project/src/page/TaskPage.tsx
+++ task-flow-project/src/page/TaskPage.tsx
@@ -1,17 +1,61 @@
-import { FilterButtons } from "../components/FilterButtons";
-import { SearchBar } from "../components/SearchBar";
-import { Summary } from "../components/Summary";
 import { TaskForm } from "../components/TaskForm";
 import { TaskList } from "../components/TaskList";
-
+import { useState } from "react";
 export function TaskPage() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [filter, setFilter] = useState<"all" | "completed" | "pending">("all");
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  type Priority = "low" | "medium" | "high";
+
+  interface Task {
+    id: number;
+    title: string;
+    priority: Priority;
+    completed: boolean;
+    createdAt: number;
+  }
+  function addTask(title: string, priority: Priority) {
+    const newTask: Task = {
+      id: Date.now(),
+      title,
+      priority,
+      completed: false,
+      createdAt: Date.now(),
+    };
+    setTasks((prev) => [...prev, newTask]);
+  }
+  function toggleCompletion(id: number) {
+    setTasks((prev) =>
+      prev.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: !task.completed };
+        }
+        return task;
+      }),
+    );
+  }
+  function deleteTasks(id: number) {
+    setTasks((prev) => prev.filter((task) => task.id !== id));
+  }
+
+  const filteredTasks = tasks
+    .filter((task) => {
+      if (filter === "completed") return task.completed;
+      if (filter === "pending") return !task.completed;
+      return true;
+    })
+    .filter((task) =>
+      task.title.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+
   return (
     <>
-      <TaskForm />
-      <FilterButtons />
-      <SearchBar />
-      <TaskList />
-      <Summary />
+      <TaskForm addTask={addTask} />
+      <TaskList
+        filteredTasks={filteredTasks}
+        toggleCompletion={toggleCompletion}
+        deleteTasks={deleteTasks}
+      />
     </>
   );
 }
